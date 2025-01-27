8116bc278deff28bbfb79add8d38447faa0ffbcd
andre4ik3 <andre4ik3@fastmail.com>
2025-01-27 13:54:21 +0400

    Capitalize header names, support for raw requests, better CSS

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
-import Handlebars from 'handlebars/runtime.js';
-import '../generated/template.mjs';
-import { hbsAsyncRender, registerAsyncHelper } from 'hbs-async-render';
+import Handlebars from "handlebars/runtime.js";
+import "../generated/template.mjs";
+import { hbsAsyncRender, registerAsyncHelper } from "hbs-async-render";
+import { capitalize } from "lodash";
 
-async function makeHTML(request: Request) {
-    const params = makeJSON(request);
-    return await hbsAsyncRender(Handlebars, 'template.html', params);
+function titleCase(str) {
+    return str.replace(/\w+/g, capitalize);
 }
 
-function makeJSON(request: Request) {
+Handlebars.registerHelper("title", titleCase);
+
+async function makeHTML(request: Request) {
+    const params = await makeJSON(request);
+    return await hbsAsyncRender(Handlebars, "template.html", params);
+}
+
+async function makeRaw(request: Request) {
+    const params = await makeJSON(request);
+    const url = new URL(request.url);
+    let data = `${params.method} ${url.pathname} ${params.protocol}\n`;
+
+    request.headers.forEach((value, key) => {
+        data += `${titleCase(key)}: ${value}\n`;
+    });
+
+    data += "\n";
+    data += params.body;
+
+    return data;
+}
+
+async function makeJSON(request: Request) {
     return {
         method: request.method,
         url: request.url,
         protocol: request.cf.httpProtocol,
         tlsVersion: request.cf.tlsVersion,
         headers: Object.fromEntries(Array.from(request.headers.entries())),
+        body: await request.text(),
+        date: new Date(),
         geo: {
-            ip: request.headers.get('CF-Connecting-IP'),
+            ip: request.headers.get("CF-Connecting-IP"),
             rtt: request.cf.clientTcpRtt,
             country: request.cf.country,
             region: request.cf.region,
@@ -31,20 +55,26 @@ function makeJSON(request: Request) {
 
 export default {
     async fetch(request, env, ctx): Promise<Response> {
-        const accept = request.headers.get('accept').split(',');
-        const html = accept.some((x) => x.startsWith('text/html'));
+        const accept = request.headers.get("accept").split(",");
 
         let body;
         let contentType;
 
-        if (accept.some((x) => x.startsWith('text/html')) && !request.url.endsWith(".json")) {
+        const extension = request.url.split(".").at(-1);
+        const acceptsHTML = accept.some(x => x.startsWith("text/html"));
+        const knownExtension = ["html", "raw", "json"].includes(extension);
+
+        if (extension === "html" || (!knownExtension && acceptsHTML)) {
             body = await makeHTML(request);
-            contentType = 'text/html';
+            contentType = "text/html";
+        } else if (extension === "raw") {
+            body = await makeRaw(request);
+            contentType = "text/plain";
         } else {
-            body = JSON.stringify(makeJSON(request));
-            contentType = 'application/json';
+            body = JSON.stringify(await makeJSON(request));
+            contentType = "application/json";
         }
 
-        return new Response(body, { headers: { 'Content-Type': contentType } });
+        return new Response(body, { headers: { "Content-Type": contentType } });
     },
 } satisfies ExportedHandler<Env>;
